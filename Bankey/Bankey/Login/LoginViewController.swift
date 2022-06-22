a1dbaf549bb1ef5c3a16d52eb03b5fdbb0a14a78
Dalton Turner <dalton@squareup.com>
2022-06-22 13:53:01 -0700

    Following lesson for loginview setup

---
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Dalton Turner on 6/22/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
+        ])
+    }
+}
+
